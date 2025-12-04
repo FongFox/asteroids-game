d96192ed3da2e57c29407ebc985438ac660abf07
FongFox <phong.tgn.coder@gmail.com>
2025-12-04 06:24:32 +0000

    Init pygame Asteroids project: print pygame version on start

---
+++ main.py
@@ -0,0 +1,11 @@
+from sys import version
+
+import pygame
+
+def main():
+    print("Hello from asteroids-game!")
+    print(f"Starting Asteroids with pygame version: {version}")
+
+
+if __name__ == "__main__":
+    main()
