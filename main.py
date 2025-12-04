514fcfc97a11f009ba2d736807c16d768e2f9158
FongFox <phong.tgn.coder@gmail.com>
2025-12-04 08:02:05 +0000

    feat: draw player ship and log position to game state

--- main.py
+++ main.py
@@ -4,33 +4,45 @@ import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
-    # print("Hello from asteroids-game!")
-    # print(f"Starting Asteroids with pygame version: {version}")
+    print("Hello from asteroids-game!")
+    print(f"Starting Asteroids with pygame version: {version}")
     print("Starting Asteroids...")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
-    pygame.init()
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
-    # Game properties
+    # 1. khởi tạo pygame, tạo screen
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     frames_rate = 60.0
     dt = 0  # stand for delta
 
-    # Game loop start
+    # 2. tạo đối tượng Player
+    player_x = SCREEN_WIDTH / 2
+    player_y = SCREEN_HEIGHT / 2
+    player = Player(player_x, player_y)
+
+    # 3. vòng lặp while game:
     while True:
+        # - Ghi log
         log_state()
+        # Thoát game nếu bắt được sự kiện
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        # - fill màn hình đen
         screen.fill("black")
+        # - vẽ player
+        player.draw(screen)
+        # - flip màn hình
         pygame.display.flip()
+        # - tính thời gian trôi qua giữa hai khung hình (dùng cho chuyển động mượt)
         dt_ms = clock.tick(frames_rate)
         dt = dt_ms / 1000
-        # print(f">>> Check delta result: {dt}")
 
 
 if __name__ == "__main__":
