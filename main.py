0dc1d949f930be737425ad29083b2778eec9815e
FongFox <phong.tgn.coder@gmail.com>
2025-12-06 07:39:57 +0000

    feat: add asteroid entities and spawning logic

--- main.py
+++ main.py
@@ -2,53 +2,64 @@ from sys import version
 
 import pygame
 
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 from player import Player
 
 
 def main():
-    print("Hello from asteroids-game!")
-    print(f"Starting Asteroids with pygame version: {version}")
-    print("Starting Asteroids...")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    # print("Hello from asteroids-game!")
+    # print(f"Starting Asteroids with pygame version: {version}")
+    # print("Starting Asteroids...")
+    # print(f"Screen width: {SCREEN_WIDTH}")
+    # print(f"Screen height: {SCREEN_HEIGHT}")
 
     # Khởi tạo pygame, tạo screen
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    frames_rate = 60.0
-    dt = 0  # stand for delta
 
-    # Tạo group
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    # Thêm player thích hợp vào group
-    Player.containers = (updatable, drawable)
+    asteroids = pygame.sprite.Group()
 
-    # Tạo đối tượng Player
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
     player_x = SCREEN_WIDTH / 2
     player_y = SCREEN_HEIGHT / 2
     player = Player(player_x, player_y)
+
+    frames_rate = 60.0
+    dt = 0  # stand for delta
 
     # Vòng lặp while game:
     while True:
         # - Ghi log
         log_state()
+
         # - Thoát game nếu bắt được sự kiện
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        # - fill màn hình đen
-        screen.fill("black")
+
         # - Cập nhật vị trí player
         updatable.update(dt)
+
+        # - fill màn hình đen
+        screen.fill("black")
+
         # - vẽ player
         for obj in drawable:
             obj.draw(screen)
+
         # - flip màn hình
         pygame.display.flip()
+
         # - tính thời gian trôi qua giữa hai khung hình (dùng cho chuyển động mượt)
         dt_ms = clock.tick(frames_rate)
         dt = dt_ms / 1000
--- player.py
+++ player.py
@@ -9,6 +9,10 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
 
+    def draw(self, screen):
+        color = "white"
+        pygame.draw.polygon(screen, color, self.triangle(), LINE_WIDTH)
+
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -17,24 +21,20 @@ class Player(CircleShape):
         c = self.position - forward * self.radius + right
         return [a, b, c]
 
-    def draw(self, screen):
-        screen_color = "white"
-        pygame.draw.polygon(screen, screen_color, self.triangle(), LINE_WIDTH)
-
-    def rotate(self, dt):
-        self.rotation = PLAYER_TURN_SPEED * dt
-
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
-            self.rotate(-1)
+            self.rotate(-dt)
         if keys[pygame.K_d]:
-            self.rotate(1)
+            self.rotate(dt)
         if keys[pygame.K_w]:
-            self.move(1)
+            self.move(dt)
         if keys[pygame.K_s]:
-            self.move(-1)
+            self.move(-dt)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
 
     def move(self, dt):
         unit_vector = pygame.Vector2(0, 1)
